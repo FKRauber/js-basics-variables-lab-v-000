e956e65f38478c940d24e0472949677864c9e752
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-06-12 03:02:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
 let mostProfitableNeighborhood = 'Chelsea';
+<<<<<<< HEAD
 let companyCeo = 'Susan Smith';
+=======
+let companyCeo = 'Susan Smith';
+>>>>>>> 79adcd06fd9533d311920c95c4bd767ea8cb3db5
